eb29cf129f4ac2344feeebf1f4d282d8098036aa
ApaarRaina <apaarraina@gmail.com>
2024-03-21 23:36:31 +0530

    third

--- main.c
+++ main.c
@@ -14,5 +14,14 @@ int binary_search(int arr[], int left, int right, int key) {
 }
 
 int main() {
-
+    int n;
+    scanf("%d",&n);
+    int num;
+    scanf("%d",&num);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",(arr+i));
+    }
+    n=binary_search(arr,0,n-1,num);
+    printf("%d",n);
 }
